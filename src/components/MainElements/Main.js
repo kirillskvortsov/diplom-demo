bfbc25f3567c1a6fa49fe5216566e4179b41cb30
kirillskvortsov <42269617+kirillskvortsov@users.noreply.github.com>
2020-06-29 17:19:41 +0300

    refactored for gh-pages

--- src/components/MainElements/Main.js
+++ src/components/MainElements/Main.js
@@ -1,63 +1,63 @@
-import React from 'react';
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from './Pages/HomePage';
-import SearchPage from './Pages/SearchPage';
-import RushPage from './Pages/RushPage';
-import PlanPage from './Pages/PlanPage';
-import SupplierPage from './Pages/SupplierPage';
-import AnalyticsPage from './Pages/AnalyticsPage';
-import IncomingPage from './Pages/IncomingPage';
-import CarParkPage from './Pages/Analytics/CarParkPage';
-import DemandPrognosisPage from './Pages/Analytics/DemandPrognosisPage';
-import OrderPointPage from './Pages/Analytics/OrderPointPage';
-import SeasonPage from './Pages/Analytics/SeasonPage';
-import SupplierCatalogPage from './Pages/Analytics/SupplierCatalogPage';
-import WarehousePage from './Pages/Analytics/WarehousePage';
+import HomePage from "./Pages/HomePage";
+import SearchPage from "./Pages/SearchPage";
+import RushPage from "./Pages/RushPage";
+import PlanPage from "./Pages/PlanPage";
+import SupplierPage from "./Pages/SupplierPage";
+import AnalyticsPage from "./Pages/AnalyticsPage";
+import IncomingPage from "./Pages/IncomingPage";
+import CarParkPage from "./Pages/Analytics/CarParkPage";
+import DemandPrognosisPage from "./Pages/Analytics/DemandPrognosisPage";
+import OrderPointPage from "./Pages/Analytics/OrderPointPage";
+import SeasonPage from "./Pages/Analytics/SeasonPage";
+import SupplierCatalogPage from "./Pages/Analytics/SupplierCatalogPage";
+import WarehousePage from "./Pages/Analytics/WarehousePage";
 
 function Main() {
-    return(
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/SearchPage">
-            <SearchPage />
-          </Route>
-          <Route path="/RushPage">
-            <RushPage />
-          </Route>
-          <Route path="/PlanPage">
-            <PlanPage />
-          </Route>
-          <Route path="/SupplierPage">
-            <SupplierPage />
-          </Route>
-          <Route path="/AnalyticsPage">
-            <AnalyticsPage />
-          </Route>
-          <Route path="/IncomingPage">
-            <IncomingPage />
-          </Route>
-          <Route path="/CarParkPage">
-            <CarParkPage />
-          </Route>
-          <Route path="/DemandPrognosisPage">
-            <DemandPrognosisPage />
-          </Route>
-          <Route path="/OrderPointPage">
-            <OrderPointPage />
-          </Route>
-          <Route path="/SeasonPage">
-            <SeasonPage />
-          </Route>
-          <Route path="/SupplierCatalogPage">
-            <SupplierCatalogPage />
-          </Route>
-          <Route path="/WarehousePage">
-            <WarehousePage />
-          </Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/HomePage">
+        <HomePage />
+      </Route>
+      <Route path="/SearchPage">
+        <SearchPage />
+      </Route>
+      <Route path="/RushPage">
+        <RushPage />
+      </Route>
+      <Route path="/PlanPage">
+        <PlanPage />
+      </Route>
+      <Route path="/SupplierPage">
+        <SupplierPage />
+      </Route>
+      <Route path="/AnalyticsPage">
+        <AnalyticsPage />
+      </Route>
+      <Route path="/IncomingPage">
+        <IncomingPage />
+      </Route>
+      <Route path="/CarParkPage">
+        <CarParkPage />
+      </Route>
+      <Route path="/DemandPrognosisPage">
+        <DemandPrognosisPage />
+      </Route>
+      <Route path="/OrderPointPage">
+        <OrderPointPage />
+      </Route>
+      <Route path="/SeasonPage">
+        <SeasonPage />
+      </Route>
+      <Route path="/SupplierCatalogPage">
+        <SupplierCatalogPage />
+      </Route>
+      <Route path="/WarehousePage">
+        <WarehousePage />
+      </Route>
+    </Switch>
+  );
 }
 
 export default Main;
--- src/components/info/navElementsInfo.js
+++ src/components/info/navElementsInfo.js
@@ -1,60 +1,60 @@
 const info = [
-    {
-        id: 1,
-        alt: 'home',
-        text: 'Домой',
-        src:'assets/home.png',
-        anchor: "/",
-        srcCard: ""
-    }, 
-    {
-        id: 2,
-        alt: 'search',
-        text: 'Поиск',
-        src:'assets/search.png',
-        anchor: "/SearchPage",
-        srcCard: "assets/background/search.png"
-    }, 
-    {
-        id: 3,
-        alt: 'rush',
-        text: 'Срочные заказы',
-        src:'assets/timer.png',
-        anchor: "/RushPage",
-        srcCard: "assets/background/rush.png"
-    }, 
-    {
-        id: 4,
-        alt: 'plan',
-        text: 'Плановые заказы',
-        src:'assets/plan.png',
-        anchor: "/PlanPage",
-        srcCard: "assets/background/plan.png"
-    }, 
-    {
-        id: 5,
-        alt: 'supplier',
-        text: 'Заказы поставщику',
-        src:'assets/delivery.png',
-        anchor: "/SupplierPage",
-        srcCard: "assets/background/supplier.png"
-    }, 
-    {
-        id: 6,
-        alt: 'incoming',
-        text: 'Приходные накладные',
-        src:'assets/incoming.png',
-        anchor: "/IncomingPage",
-        srcCard: "assets/background/incoming.png"
-    }, 
-    {
-        id: 7,
-        alt: 'analytics',
-        text: 'Аналитика',
-        src:'assets/chart.png',
-        anchor: "/AnalyticsPage",
-        srcCard: "assets/background/analytics.png"
-    }, 
+  {
+    id: 1,
+    alt: "home",
+    text: "Домой",
+    src: "assets/home.png",
+    anchor: "/HomePage",
+    srcCard: "",
+  },
+  {
+    id: 2,
+    alt: "search",
+    text: "Поиск",
+    src: "assets/search.png",
+    anchor: "/SearchPage",
+    srcCard: "assets/background/search.png",
+  },
+  {
+    id: 3,
+    alt: "rush",
+    text: "Срочные заказы",
+    src: "assets/timer.png",
+    anchor: "/RushPage",
+    srcCard: "assets/background/rush.png",
+  },
+  {
+    id: 4,
+    alt: "plan",
+    text: "Плановые заказы",
+    src: "assets/plan.png",
+    anchor: "/PlanPage",
+    srcCard: "assets/background/plan.png",
+  },
+  {
+    id: 5,
+    alt: "supplier",
+    text: "Заказы поставщику",
+    src: "assets/delivery.png",
+    anchor: "/SupplierPage",
+    srcCard: "assets/background/supplier.png",
+  },
+  {
+    id: 6,
+    alt: "incoming",
+    text: "Приходные накладные",
+    src: "assets/incoming.png",
+    anchor: "/IncomingPage",
+    srcCard: "assets/background/incoming.png",
+  },
+  {
+    id: 7,
+    alt: "analytics",
+    text: "Аналитика",
+    src: "assets/chart.png",
+    anchor: "/AnalyticsPage",
+    srcCard: "assets/background/analytics.png",
+  },
 ];
 
 export default info;
