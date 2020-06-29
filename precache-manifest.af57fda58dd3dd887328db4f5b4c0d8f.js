442468b98adf65e64ca9a2d57b88505bdc1fbb40
kirillskvortsov <42269617+kirillskvortsov@users.noreply.github.com>
2020-06-29 16:48:02 +0300

    Updates

---
+++ precache-manifest.af57fda58dd3dd887328db4f5b4c0d8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d92265595262f70613cc69dab05bd81",
+    "url": "/diplom-demo/index.html"
+  },
+  {
+    "revision": "945c87b494217533d097",
+    "url": "/diplom-demo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "56d3942a53754bbd0551",
+    "url": "/diplom-demo/static/css/main.636616c2.chunk.css"
+  },
+  {
+    "revision": "945c87b494217533d097",
+    "url": "/diplom-demo/static/js/2.15d0f95a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/diplom-demo/static/js/2.15d0f95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d3942a53754bbd0551",
+    "url": "/diplom-demo/static/js/main.bddf37f7.chunk.js"
+  },
+  {
+    "revision": "5482505119878795479d",
+    "url": "/diplom-demo/static/js/runtime-main.92c731c9.js"
+  }
+]);
